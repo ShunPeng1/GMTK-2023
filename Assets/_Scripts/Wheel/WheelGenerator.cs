aa4bf712c23bd3a30aa26a0d3988302700f0bff3
ShunPeng1 <91878382+ShunPeng1@users.noreply.github.com>
2023-07-08 08:12:25 +0700

    feat: order of wheel

--- Assets/_Scripts/Wheel/WheelGenerator.cs
+++ Assets/_Scripts/Wheel/WheelGenerator.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class WheelGenerator : MonoBehaviour
@@ -45,5 +46,31 @@ public class WheelGenerator : MonoBehaviour
         // Calculate the angle for each arc
         float arcAngle = (360f / numArcs) - spacing;
         return arcAngle;
+    }
+    
+    
+    public bool removeExistingColliders = true;
+ 
+    public void CreateInvertedMeshCollider()
+    {
+        if (removeExistingColliders)
+            RemoveExistingColliders();
+ 
+        InvertMesh();
+ 
+        gameObject.AddComponent<MeshCollider>();
+    }
+ 
+    private void RemoveExistingColliders()
+    {
+        Collider[] colliders = GetComponents<Collider>();
+        for (int i = 0; i < colliders.Length; i++)
+            DestroyImmediate(colliders[i]);
+    }
+ 
+    private void InvertMesh()
+    {
+        Mesh mesh = GetComponent<MeshFilter>().mesh;
+        mesh.triangles = mesh.triangles.Reverse().ToArray();
     }
 }
