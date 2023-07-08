ed6f69e01bb1d2a5e5618e7bdbb4303e6af3da18
ShunPeng1 <91878382+ShunPeng1@users.noreply.github.com>
2023-07-08 21:35:08 +0700

    feat: swap role

--- Assets/_Scripts/Cards/CardsInformation/SwitchRoleCardInformation.cs
+++ Assets/_Scripts/Cards/CardsInformation/SwitchRoleCardInformation.cs
@@ -5,16 +5,18 @@ using UnityEngine;
 
 namespace _Scripts.Cards.CardsInformation
 {
-    [CreateAssetMenu(fileName = "Card Information", menuName = "Card Information/Attack Verb" )]
-    public class AttackCardInformation : BaseCardInformation
+    
+    [CreateAssetMenu(fileName = "Card Information", menuName = "Card Information/Switch Role")]
+    public class SwitchRoleCardInformation : BaseCardInformation
     {
+        [SerializeField] private ActorRole _actorRole;
         public override void Awake()
         {
             WordCardType = WordCardType.VerbBinary;
             Priority = 0;
-            Name = " Is Reduced by ";
+            Name = " Switch Role to";
             Cost = 1;
-            ParametersCount = 2;
+            ParametersCount = 0;
             ResultCount = 1;
         }
         
@@ -31,7 +33,7 @@ namespace _Scripts.Cards.CardsInformation
             {
                 var actorBehavior1 = (ActorBehavior)parameters1[0];
                 var actorBehavior2 = (ActorBehavior)parameters2[0];
-                actorBehavior1.Attack(actorBehavior2);
+                actorBehavior1.Role.Value = GetRole(actorBehavior2);
             }
             else if (dataType1 == typeof(ActorBehavior []) && dataType2 == typeof(ActorBehavior))
             {
@@ -39,34 +41,33 @@ namespace _Scripts.Cards.CardsInformation
                 var actorBehavior2 = (ActorBehavior)parameters2[0];
                 foreach (var actor in actorBehaviors1)
                 {
-                    actor.Attack(actorBehavior2);
+                    actor.Role.Value = GetRole(actorBehavior2);
                 }
             }
             else if (dataType1 == typeof(ActorBehavior) && dataType2 == typeof(ActorBehavior []))
             {
                 var actorBehavior1 = (ActorBehavior)parameters1[0];
                 var actorBehaviors2 = (ActorBehavior [])parameters2[0];
-                foreach (var actor in actorBehaviors2)
-                {
-                    actorBehavior1.Attack(actor);
-                }
+                
+                actorBehavior1.Role.Value = GetRole(actorBehaviors2[0]);
             }
             else if (dataType1 == typeof(ActorBehavior []) && dataType2 == typeof(ActorBehavior []))
             {
                 var actorBehaviors1 = (ActorBehavior[])parameters1[0];
                 var actorBehaviors2 = (ActorBehavior[])parameters2[0];
-                foreach (var actor1 in actorBehaviors1)
+               
+                foreach (var actor in actorBehaviors1)
                 {
-                    foreach (var actor2 in actorBehaviors2)
-                    {
-                        actor1.Attack(actor2);
-                    }
+                    actor.Role.Value = GetRole(actorBehaviors2[0]);
                 }
             }
             
             return new object[] { };
         }
 
-    
+        private ActorRole GetRole(ActorBehavior actorBehavior)
+        {
+            return actorBehavior.Role.Value;
+        }
     }
 }
