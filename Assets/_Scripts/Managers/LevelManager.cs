fc09183c898112b648db958ff31f8af8fae85879
ShunPeng1 <91878382+ShunPeng1@users.noreply.github.com>
2023-07-09 12:54:44 +0700

    feat: background

---
+++ Assets/_Scripts/Managers/LevelManager.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using _Scripts.Cards;
+using DG.Tweening;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityUtilities;
+
+public class LevelManager : MonoBehaviour
+{
+    public enum WhoseTurn
+    {
+        Ally,
+        Enemy
+    }
+
+    [Serializable]
+    public class EnemySentence
+    {
+    }
+    
+    private int _turn;
+    
+    [SerializeField] private BaseCardInformation[] _allyCards;
+    [SerializeField] private List<BaseCardInformation>[] _enemySentences;
+    
+    private RandomBag<BaseCardInformation> _allyBag;
+    private RandomBag<BaseCardInformation> _enemyBag;
+
+    public Action NextBattleFieldAnimations;
+
+    
+    [Header("Craft Bench Animation")]
+    [SerializeField] private GameObject _upperCraftBench;
+    [SerializeField] private GameObject _lowerCraftBench;
+    [SerializeField] private float _transitionDuration;
+    [SerializeField] private Ease _transitionEase = Ease.OutCubic;
+    [SerializeField] private Vector3 _transitionOffsetPosition = new Vector3(0, 10, 0);
+    
+    
+    void ShowBattleField()
+    {
+        NextBattleFieldAnimations.Invoke();
+    }
+
+    void EnemyTurn()
+    {
+        
+    }
+    
+    void ShowCraftBench()
+    {
+        _upperCraftBench.transform.DOMove(-_transitionOffsetPosition, _transitionDuration).SetRelative()
+            .SetEase(_transitionEase);
+        
+        _lowerCraftBench.transform.DOMove(_transitionOffsetPosition, _transitionDuration).SetRelative()
+            .SetEase(_transitionEase);
+    }
+
+    void HideCraftBench()
+    {
+        _upperCraftBench.transform.DOMove(_transitionOffsetPosition, _transitionDuration).SetRelative()
+            .SetEase(_transitionEase);
+        
+        _lowerCraftBench.transform.DOMove(-_transitionOffsetPosition, _transitionDuration).SetRelative()
+            .SetEase(_transitionEase);
+    }
+}
