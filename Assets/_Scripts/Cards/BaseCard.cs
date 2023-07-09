6947042674392a8558ce603ccde75ec045eb18d3
ShunPeng1 <91878382+ShunPeng1@users.noreply.github.com>
2023-07-09 09:01:00 +0700

    fix: base card valide remove

--- Assets/_Scripts/Cards/BaseCard.cs
+++ Assets/_Scripts/Cards/BaseCard.cs
@@ -11,12 +11,12 @@ public class BaseCard : MonoBehaviour
 
     [SerializeField] private SpriteRenderer _spriteRenderer;
     [SerializeField] private TMP_Text _wordText;
-    private void OnValidate()
+    /*private void OnValidate()
     {
         _wordText.text = CardInformation.Name;
         
         if (VisualManager.Instance == null) return;
         _spriteRenderer.color = VisualManager.Instance.GetColorCard(CardInformation.WordCardType);
         _spriteRenderer.sprite = VisualManager.Instance.GetSpriteCard(CardInformation.WordCardType);
-    }
+    }*/
 }
